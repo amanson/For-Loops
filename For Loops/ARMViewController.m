113aea61fac534a197d60cb1319a7f0435938b70
Andy Manson <amanson@iMac-105.local>
2014-09-24 15:55:25 -0700

    add Git comment

--- For Loops/ARMViewController.m
+++ For Loops/ARMViewController.m
@@ -18,6 +18,25 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+//    for (int meditationHours = 1; meditationHours <= 100; meditationHours ++){
+//        NSLog(@"I am getting more enlightened");
+//    }
+//    int strandOfWheat = 1;
+//    for (int i = 2; i <= 30; i++){
+//        strandOfWheat = strandOfWheat * 2;
+//        NSLog(@"%i", strandOfWheat);
+//    }
+    // Testing commit to Git
+
+    for (int bottlesOfBeer = 99; bottlesOfBeer >= 0; bottlesOfBeer--){
+        if (bottlesOfBeer != 0) {
+            NSLog(@"%i Bottles of Beer", bottlesOfBeer);
+        }
+        else
+            NSLog(@"You drank them all");       
+       
+    }
+    
 }
 
 - (void)didReceiveMemoryWarning
